e7f8aef492c44df91a40de516822786ca88d3f47
Hady Farahat <farhat2312@gmail.com>
2021-10-31 13:02:23 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import imageInSrc from "./imageInSrc.png";
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        className="wholeThing"
+        style={{ border: "solid 1px black", maxWidth: "100vw" }}
+      >
+        <span>
+          <h1 className="title red">Hady Ahmed</h1>
+        </span>
+        <br />
+        <img src={imageInSrc} alt={"no thing to preview"} />
+        <br />
+        <img src="/imageInPublic.png" alt={"no thing to preview"} />
+
+        <video style={{ width: "320", height: "240" }} controls>
+          <source src="/myVideo.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </>
   );
 }
 
